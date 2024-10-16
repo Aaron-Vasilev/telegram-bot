fb7637f63fd2415cbd89dfc95782111c584b9e9c
Aaron Vasilev <aaron.vasilev@entro.security>
2024-10-16 18:28:00 +0300

    Moved bot to lambda

--- go.mod
+++ go.mod
@@ -9,4 +9,7 @@ require (
 
 require github.com/google/uuid v1.6.0
 
-require github.com/robfig/cron/v3 v3.0.1 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -6,12 +6,17 @@ import (
 	"bot/src/handler"
 	"bot/src/scene"
 	"bot/src/utils"
+	t "bot/src/utils/types"
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"database/sql"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	_ "github.com/lib/pq"
 )
 
@@ -39,14 +44,29 @@ func main() {
 	cron.Cron(bot, db)
 
 	fmt.Println("Launch!")
-	defer func() {
-		if r := recover(); r != nil {
-			bot.Error("Panic! " + r.(string))
-		}
-	}()
-	for {
-		updates := bot.GetUpdates()
 
-		handler.HandleUpdates(&ctx, bot, db, updates)
+	if bot.IsDebug {
+		for {
+			updates := bot.GetUpdates()
+
+			handler.HandleUpdates(&ctx, bot, db, updates)
+		}
+	} else {
+		lambda.Start(func(c context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+			var update t.Update
+
+			err := json.Unmarshal([]byte(req.Body), &update)
+
+			if err != nil {
+				fmt.Println("Failed to parse request body:", err)
+				bot.Error(err.Error())
+
+				return events.APIGatewayProxyResponse{StatusCode: 400}, nil
+			}
+
+			handler.HandleUpdates(&ctx, bot, db, []t.Update{update})
+
+			return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+		})
 	}
 }
