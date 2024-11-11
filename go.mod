ffa15560050df46b95a5140a90dbbee828e5b579
Aaron Vasilev <aaron.vasilev@entro.security>
2024-11-11 18:03:58 +0200

    Splitted business and bot logic

--- go.mod
+++ go.mod
@@ -1,13 +1,3 @@
 module bot
 
 go 1.23
-
-require (
-	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
-)
-
-require (
-	github.com/google/uuid v1.6.0
-	github.com/robfig/cron/v3 v3.0.1
-)
--- src/bot/bot.go
+++ src/bot/bot.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"bot/src/scene"
 	t "bot/src/utils/types"
 	"bytes"
 	"encoding/json"
@@ -8,28 +9,43 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
+	"strings"
 )
+
+type callback func(ctx *scene.Ctx, bot *Bot, u t.Update)
+type menuCb func(bot *Bot, u t.Update)
+
+type command struct {
+	regex string
+	cb callback
+}
 
 type Bot struct {
 	Token    string
 	IsDebug  bool
 	LogLevel int
 	Offset   int
+
+	CBQueries []command
+	SceneMap map[string]callback
+	KeyboardMap map[string]callback
+	MenuMap map[string]menuCb
 }
 
 func (bot *Bot) GetMe() t.TBot {
-	return Call[t.TBot](bot, "/getMe")
+	return call[t.TBot](bot, "/getMe")
 }
 
 func (bot *Bot) GetUpdates() []t.Update {
 	offset := strconv.Itoa(bot.Offset)
 
-	return Call[[]t.Update](bot, "/getUpdates?timeout=1&offset="+offset)
+	return call[[]t.Update](bot, "/getUpdates?timeout=1&offset="+offset)
 }
 
 func (bot *Bot) SendMessage(msg t.Message) {
-	Send(bot, "/sendMessage", msg)
+	send(bot, "/sendMessage", msg)
 }
 
 func (bot *Bot) SendText(chatId int64, text string) {
@@ -38,7 +54,7 @@ func (bot *Bot) SendText(chatId int64, text string) {
 		ChatId: chatId,
 	}
 
-	Send(bot, "/sendMessage", msg)
+	send(bot, "/sendMessage", msg)
 }
 
 func (bot *Bot) SendHTML(chatId int64, text string) {
@@ -48,7 +64,7 @@ func (bot *Bot) SendHTML(chatId int64, text string) {
 		ParseMode: "html",
 	}
 
-	Send(bot, "/sendMessage", msg)
+	send(bot, "/sendMessage", msg)
 }
 
 func (bot *Bot) Forward(chatId, fromChatId int64, msgId int) {
@@ -58,7 +74,7 @@ func (bot *Bot) Forward(chatId, fromChatId int64, msgId int) {
 		FromChatID: fromChatId,
 	}
 
-	Send(bot, "/forwardMessage", msg)
+	send(bot, "/forwardMessage", msg)
 }
 
 func (bot *Bot) SendSticker(chatId int64, stickerId string) {
@@ -67,7 +83,7 @@ func (bot *Bot) SendSticker(chatId int64, stickerId string) {
 		Sticker: stickerId,
 	}
 
-	Send(bot, "/sendSticker", msg)
+	send(bot, "/sendSticker", msg)
 }
 
 func (bot *Bot) SendPhotoById(chatId int64, fileId string) {
@@ -76,7 +92,7 @@ func (bot *Bot) SendPhotoById(chatId int64, fileId string) {
 		Photo:  fileId,
 	}
 
-	Send(bot, "/sendPhoto", msg)
+	send(bot, "/sendPhoto", msg)
 }
 
 func (bot *Bot) SendLocation(chatId int64, lat float32, long float32) {
@@ -86,7 +102,7 @@ func (bot *Bot) SendLocation(chatId int64, lat float32, long float32) {
 		Longitude: long,
 	}
 
-	Send(bot, "/sendLocation", msg)
+	send(bot, "/sendLocation", msg)
 }
 
 func (bot *Bot) Error(text string) {
@@ -103,7 +119,7 @@ func (bot *Bot) Error(text string) {
 	}
 }
 
-func Send(bot *Bot, method string, msg t.Message) {
+func send(bot *Bot, method string, msg t.Message) {
 	var resData t.Response[t.Message]
 	jsonData, err := json.Marshal(msg)
 
@@ -145,7 +161,7 @@ func Send(bot *Bot, method string, msg t.Message) {
 	}
 }
 
-func Call[T any](bot *Bot, method string) T {
+func call[T any](bot *Bot, method string) T {
 	var resData t.Response[T]
 	res, err := http.Get("https://api.telegram.org/bot" + bot.Token + method)
 
@@ -178,4 +194,109 @@ func Call[T any](bot *Bot, method string) T {
 	}
 
 	return resData.Result
+}
+
+func (bot *Bot) Launch() {
+	ctx := scene.NewSceneContext()
+
+	fmt.Println("Launch!")
+
+	for {
+		updates := bot.GetUpdates()
+
+		handleUpdates(&ctx, bot, updates)
+	}
+}
+
+func (bot *Bot) NewCallbackQuery(regex string, cb callback) error {
+	if _, err := regexp.Compile(regex); err != nil {
+		return err
+	}
+
+	bot.CBQueries = append(bot.CBQueries, command{
+		regex: regex,
+		cb: cb,
+	})
+
+	return nil
+}
+
+func handleUpdates(c *scene.Ctx, bot *Bot, updates []t.Update) {
+	for _, update := range updates {
+		handleUpdate(c, bot, update)
+
+		bot.Offset = update.UpdateID + 1
+	}
+}
+
+func handleCallbackQuery(ctx *scene.Ctx, bot *Bot, u t.Update) {
+	for _, cmd := range bot.CBQueries {
+		if regexp.MustCompile(cmd.regex).Match([]byte(u.CallbackData())) {
+			cmd.cb(ctx, bot, u)
+		}
+	}
+}
+
+func (bot *Bot) NewScene(sceneName string, cb callback) {
+	bot.SceneMap[sceneName] = cb
+}
+
+func (bot *Bot) StartScene(ctx *scene.Ctx, u t.Update, sceneName string) {
+	ctx.SetValue(u.FromChat().ID, scene.SceneState{
+		Scene: sceneName,
+		Stage: 1,
+	})
+
+	bot.SceneMap[sceneName](ctx, bot, u)
+}
+
+func handleScene(ctx *scene.Ctx, bot *Bot, u t.Update) {
+	state, _ := ctx.GetValue(u.FromChat().ID)
+
+	bot.SceneMap[state.Scene](ctx, bot, u)
+}
+
+func (bot *Bot) NewMenuItem(item string, cb menuCb) {
+	bot.MenuMap[item] = cb
+}
+
+func handleMenu(bot *Bot, u t.Update) {
+	cb, ok := bot.MenuMap[u.Message.Text]
+
+	if ok {
+		cb(bot, u)
+	}
+}
+
+func handleUpdate(ctx *scene.Ctx, bot *Bot, u t.Update) {
+	if u.FromChat() == nil || (u.Message != nil && strings.HasPrefix(u.Message.Text, "/")) {
+		handleMenu(bot, u)
+
+		return
+	}
+
+	userId, updateWithCallbackQuery := userIdFromUpdate(u)
+
+	_, ok := ctx.GetValue(userId)
+
+	if ok {
+		handleScene(ctx, bot, u)
+	} else if updateWithCallbackQuery {
+		handleCallbackQuery(ctx, bot, u)
+	} else if strings.HasPrefix(u.Message.Text, "/") {
+		handleMenu(bot, u)
+	}
+}
+
+func userIdFromUpdate(u t.Update) (int64, bool) {
+	var userId int64
+	updateWithCallbackQuery := u.CallbackQuery != nil
+
+	if updateWithCallbackQuery {
+		userId = u.CallbackQuery.From.ID
+	} else if u.Message != nil {
+		userId = u.Message.From.ID
+	}
+
+	return userId, updateWithCallbackQuery
 }
